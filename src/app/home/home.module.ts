fc6e9bb8d2779b86b05b7c2c63d7dd9ee141ae30
Markollantes2307 <u201410183@upc.edu.pe>
2023-05-27 19:46:19 -0500

    feat: recruiters notifications v1

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { MaterialModule } from '../material/material.module';
+import { NotificationsComponent } from './pages/notifications/notifications.component';
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, NotificationsComponent],
   imports: [CommonModule, HomeRoutingModule, MaterialModule],
 })
 export class HomeModule {}
