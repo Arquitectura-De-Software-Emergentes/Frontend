a13b8551d575569ea2c9f1d8c09e1bc16f61347d
Markollantes2307 <72058326+Markollantes2307@users.noreply.github.com>
2023-06-01 18:29:06 -0500

    feat: routing recruiter profile

---
+++ src/app/iam/iam-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { RecruiterProfileComponent } from './recruiter-profile/recruiter-profile.component';
+
+const routes: Routes = [
+    {
+      path: '',
+      component: RecruiterProfileComponent
+    },
+  ]
+  @NgModule({
+    declarations: [],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+  })
+  export class IamRoutingModule { }
