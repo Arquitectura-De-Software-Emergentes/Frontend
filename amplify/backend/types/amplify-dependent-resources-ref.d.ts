a132aec408942c169f8de54cb097327753f486a2
Omar Jesus Cahuana Rios <omar_jesus23@hotmail.com>
2023-06-11 17:18:21 -0500

    configuration for deployments

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api544a155c": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "teacherfinder417995a7": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
---
+++ src/enviroments/enviroment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://ec2-3-95-18-5.compute-1.amazonaws.com:8080/',
+    apiKey: 'YOUR_API_KEY',
+    // Otras variables de entorno específicas para producción
+  };
