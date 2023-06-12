6e8d3cc42a1d52e12b4b384a7d10cf35ac5546d8
Omar Jesus Cahuana Rios <omar_jesus23@hotmail.com>
2023-06-11 19:07:05 -0500

    changes for dits

--- src/app/application/services/applicant.service.ts
+++ src/app/application/services/applicant.service.ts
@@ -7,7 +7,7 @@ import { JobOffer } from 'src/app/assessment/models/jobOffer';
   providedIn: 'root'
 })
 export class ApplicantService {
-  readonly apiUrl: string = 'http://ec2-3-95-18-5.compute-1.amazonaws.com:8080/job-offers/'
+  readonly apiUrl: string = 'https://ec2-3-95-18-5.compute-1.amazonaws.com:8080/job-offers/'
   constructor(private http:HttpClient) { }
 
   getJobOffers():Observable<JobOffer[]>{
